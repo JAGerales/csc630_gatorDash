bb1f43a0565b12907afbadae3accdda34989bc9b
unknown <jacob.a.gerales@gmail.com>
2024-04-08 19:45:22 -0700

    Animation for movement and jump bug fix

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,21 +4,49 @@ public class playerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
-
+    private Animator anim;
+    private bool grounded;
     private void Awake()
     {
-        // Reference Rigidbody2D Component
+        // Reference Rigidbody2D Component and animator Component
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         if (body != null)
         {
-            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); // Transforms on X axis
+            float horizontalInput = Input.GetAxis("Horizontal");
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y); // Transforms on X axis
 
-            if (Input.GetKey(KeyCode.Space))
-                body.velocity = new Vector2(body.velocity.x, speed); // Transforms on Y axis
+            // flips player when moving right or left
+            if (horizontalInput > 0.01f)
+                transform.localScale = new Vector3(0.65f, 0.65f, 0.65f);
+            else if (horizontalInput < -0.01f)
+                transform.localScale = new Vector3(-0.65f, 0.65f, 0.65f);
+
+            if (Input.GetKey(KeyCode.Space) && grounded)
+                Jump();
+
+            // Set animator parameters
+            anim.SetBool("isRun", horizontalInput != 0); // checks to see if horizontal input is zero or not
+            anim.SetBool("isGrounded", grounded);
+        }
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed / 2); // Transforms on Y axis
+        anim.SetTrigger("Jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 }
